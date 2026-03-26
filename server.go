b20603cf640ab205bfac7f0a5abdc146f841937a
sakura1314lyc <1951521387@qq.com>
2026-03-26 23:16:31 +0800

    完善用户结构体，添加用户上线、下线及消息处理功能

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -55,15 +56,32 @@ func (this *Server) Handler(conn net.Conn) {
 	//.当前链接的业务
 	//fmt.Println("链接建立成功")
 
-	user := NewUser(conn)
+	user := NewUser(conn, this) // 创建用户
 
-	//用户上线 将用户加入到onlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
+	user.Online() // 用户上线
 
-	//广播当前用户上线消息
-	this.BroadCast(user, "已上线")
+	//接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				user.Offline() //用户下线
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			//提取用户的消息(去除回车)
+			msg := string(buf[:n-1])
+
+			//将得到的数据进行处理
+			user.DoMessage(msg)
+		}
+	}()
 
 	//当前handler阻塞
 	select {}
--- user.go
+++ user.go
@@ -7,21 +7,54 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+
+	Server *Server
 }
 
 //创建用户的API
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		C:    make(chan string),
-		conn: conn,
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
+		Server: server,
 	}
 
 	//启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
+
 	return user
+}
+
+//用户上线的业务
+func (this *User) Online() {
+
+	//用户上线 将用户加入到onlineMap中
+	this.Server.mapLock.Lock()
+	this.Server.OnlineMap[this.Name] = this
+	this.Server.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.Server.BroadCast(this, "已上线")
+}
+
+//用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+	this.Server.BroadCast(this, msg)
+}
+
+//用户下线的业务
+func (this *User) Offline() {
+
+	//用户下线 将用户从onlineMap中删除
+	this.Server.mapLock.Lock()
+	delete(this.Server.OnlineMap, this.Name)
+	this.Server.mapLock.Unlock()
+
+	//广播当前用户下线消息
+	this.Server.BroadCast(this, "已下线")
 }
 
 //监听当前User channel的方法，一旦有消息，就直接发送给对端客户端
