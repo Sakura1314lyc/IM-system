83cde2f2e0e62d0852e27371c369939384cb248b
sakura1314lyc <1951521387@qq.com>
2026-03-28 20:30:40 +0800

    更新了不同业务处理模式

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int //表示当前client的模式
 }
 
 func NewClient(serverip string, serverport int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverip string, serverport int) *Client {
 	client := &Client{
 		Serverip:   serverip,
 		ServerPort: serverport,
+		flag:       99,
 	}
 
 	//链接server
@@ -31,6 +33,45 @@ func NewClient(serverip string, serverport int) *Client {
 
 	//返回对象
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>请输入合法范围内的数字<<<<<")
+		return false
+	}
+}
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+
+		}
+
+		//根据不同模式处理不同的业务
+		switch client.flag {
+		case 1:
+			//公聊模式
+			fmt.Println("进入公聊模式...")
+		case 2:
+			//私聊模式
+			fmt.Println("进入私聊模式...")
+		case 3:
+			//更新用户名
+			fmt.Println("更新用户名...")
+			//go的switch默认break
+		}
+	}
 }
 
 var serverIp string
@@ -55,5 +96,5 @@ func main() {
 	fmt.Println(">>>>>连接服务器成功...")
 
 	//启动客户端业务
-	select {}
+	client.Run()
 }
